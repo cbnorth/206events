95ca0f45c4aa5a76a4047c53f87923cca2158b22
Chris North <chrisburkenorth@gmail.com>
2018-10-30 07:46:35 -0700

    updated services and events mobile layout

--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -4,33 +4,14 @@ import './events.scss';
 import Weddings from './Weddings.js';
 import PrivateParties from './PrivateParties';
 import CorporateEvents from './CorporateEvents.js';
-import MediaQuery from 'react-responsive'
-import Slider from "react-slick";
 
 class Events extends Component {
     render() {
-        const settings = {
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: true,
-        };
-
         return (
             <div>
-                <MediaQuery query="(max-width: 767px)">
-                    <Slider {...settings}>
-                        <Weddings />
-                        <CorporateEvents />
-                        <PrivateParties />
-                    </Slider>
-                </MediaQuery>
-                <MediaQuery query="(min-width: 768px)">
-                    <Weddings />
-                    <CorporateEvents />
-                    <PrivateParties />
-                </MediaQuery>
+                <Weddings />
+                <CorporateEvents />
+                <PrivateParties />
             </div>
         );
     }
